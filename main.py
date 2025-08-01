38fdc5250358013e00ec9058d6f6b19b06158c86
chrisanderson16 <anderson.chriz@gmail.com>
2025-08-01 00:19:43 -0300

    dd

--- main.py
+++ main.py
@@ -31,9 +31,6 @@ dir_img = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'img')
 #OS PATH to font directory
 dir_font = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fonts')
 
-#OS PATH to events directory
-dir_events = os.path.join(os.path.realpath(__file__), 'events')
-
 newIconSize = (250, 250)
 WHITE = (255, 255, 255, 255)
 BLACK = (0, 0, 0, 255)
@@ -189,6 +186,9 @@ try:
         # Individual calendar lines
     
     
+    #OS PATH to events directory
+    dir_events = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'events')
+
 
 # This step needs to be replaced with eventHandler.py
 #   Currently, the format is (date, event) as a string -> event = f"{datetime} {event}" (This is iterated through a for loop and is appended to the return)
